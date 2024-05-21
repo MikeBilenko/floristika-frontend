44f8c3aebb0cd0eeec4a7d3fcbe2620a486933f2
mike <mike@cdontiy.com>
2024-05-21 23:09:23 +0300

    updated styles a bit:

--- src/components/Checkout/CheckoutWrapperConfirm/CheckoutWrapperConfirm.jsx
+++ src/components/Checkout/CheckoutWrapperConfirm/CheckoutWrapperConfirm.jsx
@@ -118,6 +118,7 @@ const CheckoutWrapperConfirm = () => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
       navigate("/cart/");
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
